908db27a0b19eb236e7ba23b3400554c8ab9f94c
Kerbx <r3ostat@ya.ru>
2022-01-21 11:00:14 +0300

    Created class for resources loading, class for magician (only __init__)

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import pyxel
 
 
-class App():
+#############
+# CONSTANTS #
+#############
+
+FPS = 30
+TITLE = 'RPG'
+SIZE_W = 300
+SIZE_H = 300
+RESOURCES_FILENAME = 'ale.pyxres'
+
+
+###########
+# CLASSES #
+###########
+
+class OnLoadResourses():
+    """This class using for loading resources (images, tilemap, music etc.)
+    """
     def __init__(self) -> None:
-        self.x = 0
-        self.y = 0
-        pyxel.init(100, 100, fps=60)
-        pyxel.load('ale.pyxres')
+        pyxel.load(RESOURCES_FILENAME)
+        
+        
+class Mag():
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+        self.name = 'Magician'
+
+
+class App():
+    """Class with app definition and initialization.
+    """
+    def __init__(self) -> None:
+        pyxel.init(width=SIZE_W, height=SIZE_H, title=TITLE, fps=FPS)
         pyxel.run(self.update, self.draw)
         
     def update(self):
-        self.x += 1
+        pass
     
     def draw(self):
         pyxel.cls(0)
-        pyxel.blt(0, 0, 0, 0, 0, 15, 15, 0)
         
 App()
         
