76f962f1773a74126cfcc3e21b2e875d1cd5cd47
Kerbx <r3ostat@ya.ru>
2022-01-23 15:40:32 +0300

    Added class for Dialog System, doesnt work well. Needs to be remaked.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pyxel
 
 
-########################
-# CONSTANTS FOR WINDOW #
-########################
+#############################
+# CONSTANTS FOR APPLICATION #
+#############################
 
 FPS = 30
 TITLE = 'RPG'
@@ -17,12 +17,57 @@ RESOURCES_FILENAME = 'ale.pyxres'
 ########################
 
 COLOR_KEY = 0 # Color to wrap background.
+DIALOG_WIDTH = 256
+DIALOG_HEIGHT = 64
+
+
+###############################
+# CONSTANTS FOR DIALOG SYSTEM #
+###############################
+
+DIALOG_TILEMAP = 0
+DIALOG_U = 0
+DIALOG_V = 0
+DIALOG_POS_X = 0
+DIALOG_POS_Y = 236
+DIALOG_COLOR_KEY = 0
+DIALOG_COLOR = 4
+NARRATIVE_SYMBOL = '#'
+QUESTION_SYMBOL = '?'
+ANSWER_SYMBOL = '&'
 
 
 ###########
 # CLASSES #
 ###########
 
+class DialogSystem():
+    """This class simulating dialog system.
+    """
+    def __init__(self, file) -> None:
+        self.dialog_file = file
+        pyxel.bltm(DIALOG_POS_X, DIALOG_POS_Y, DIALOG_TILEMAP, DIALOG_U, DIALOG_V, DIALOG_WIDTH, DIALOG_HEIGHT, DIALOG_COLOR_KEY)
+    
+    def narrative(self) -> None:
+        with open(self.dialog_file) as dialog_file:
+            dialog_answers = list(filter(lambda string: string.startswith(ANSWER_SYMBOL), list(dialog_file)))
+            for row in dialog_file:
+                print(row)
+                if pyxel.btn(pyxel.KEY_E):
+                    if row.startswith(NARRATIVE_SYMBOL):
+                        pyxel.text(6, 240, row[1:], DIALOG_COLOR)
+                    elif row.startswith(QUESTION_SYMBOL):
+                        pyxel.text(6, 240, row[1:], DIALOG_COLOR)
+                        for answers in dialog_answers:
+                            a = answers
+                            answers = answers[1:-1]
+                            answers = answers.split(sep='|')
+                            i = 1
+                            for answer in answers:
+                                pyxel.text(6, 240 + 6 * i, str(i) + ' ' + answer, DIALOG_COLOR)
+                            dialog_answers.remove(a)   
+                
+                
 class OnLoadResourses():
     """This class using for loading resources (images, tilemap, music etc.)
     """
@@ -37,10 +82,10 @@ class Mag():
         self.u = 0
         self.v = 0
         self.image = 0
-        self.height = 15
-        self.width = 15
+        self.height = 16
+        self.width = 16
         self.name = 'Magician'
-        self.persona = pyxel.Image(15, 15)
+        self.persona = pyxel.Image(self.width, self.height)
         
         if color == 'yellow':
             self.u = 0
@@ -54,7 +99,8 @@ class Mag():
         else:
             pass
         
-        self.persona = pyxel.blt(self.x, self.y, self.image, self.u, self.v, self.width, self.height, COLOR_KEY)            
+        self.persona = pyxel.blt(self.x, self.y, self.image, self.u, self.v, self.width, self.height, COLOR_KEY)
+        
     def draw(self, x, y):
         self.persona = pyxel.blt(x, y, self.image, self.u, self.v, self.width, self.height, COLOR_KEY)
         
@@ -67,15 +113,14 @@ class App():
         OnLoadResourses()
         self.x = 0
         self.mag1 = Mag(0, 0, 'yellow')
+        self.dia = DialogSystem('dialogSystem/magician.txt')
         pyxel.run(self.update, self.draw)
-        
     def update(self):
-        pass
+        self.dia.narrative()
     def draw(self):
         pyxel.cls(0)
         self.mag1.draw(self.x, 0)
-        
-        
+    
 
 App()
         
